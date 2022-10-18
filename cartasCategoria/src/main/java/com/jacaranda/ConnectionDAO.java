eff449d9766809b0e7e2c8790a7c49577c83fca8
RomanMillan <romandos962m@gmail.com>
2022-10-18 09:40:14 +0200

    first commit

---
+++ cartasCategoria/src/main/java/com/jacaranda/ConnectionDAO.java
@@ -0,0 +1,19 @@
+package com.jacaranda;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class ConnectionDAO {
+	private StandardServiceRegistry sr ;
+	private SessionFactory sf;
+	private Session session;
+	
+	public ConnectionDAO() {
+		sr = new StandardServiceRegistryBuilder().configure().build();
+		sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
+		session = sf.openSession();
+	}
+}
