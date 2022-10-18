e8fe8439aa0ccf6a69b053218e464595e68ac7b7
casti-lob <casti_lob@hotmail.com>
2022-10-18 18:05:04 +0200

    Clase para la conexion y metodos add y delete

--- cartasCategoria/src/main/java/com/jacaranda/ConnectionDAO.java
+++ cartasCategoria/src/main/java/com/jacaranda/ConnectionDAO.java
@@ -16,4 +16,34 @@ public class ConnectionDAO {
 		sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
 		session = sf.openSession();
 	}
+	
+	//Metodos para clase Card
+	public void addCard(Card c) {
+		session.getTransaction().begin();
+		session.save(c);
+		session.getTransaction().commit();
+	}
+	
+	public void deleteCard(int id) {
+		Card c = (Card) session.get(Card.class,id);
+		session.getTransaction().begin();
+		session.delete(c);
+		session.getTransaction().commit();
+	}
+	
+	
+	//Metodos para clase Category 
+	public void addCategory(Category cat) {
+		session.getTransaction().begin();
+		session.save(cat);
+		session.getTransaction().commit();
+	}
+	
+	public void deleteCategory(String nameCategory) {
+		Category cat = (Category) session.get(Category.class,nameCategory);
+		session.getTransaction().begin();
+		session.delete(cat);
+		session.getTransaction().commit();
+	}
+	
 }
